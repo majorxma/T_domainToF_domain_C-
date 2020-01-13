406f46a1122edc69d71144b712d5d7e0bba7fb64
majorxma <majorxma@163.com>
2020-01-13 18:47:34 +0800

    Initial

---
+++ T_domain2F_domain/T_domain2F_domain/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace T_domain2F_domain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+}
